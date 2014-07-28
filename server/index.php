bd47e25f08352f816c7880f9a8bc785abda39e89
Harri Bell-Thomas <harribt@live.co.uk>
2014-07-28 20:17:54 +0100

    Server Request Automation, Extended Request Class
    New Request Class functions, developed MainExecuteRequest() function
    (#badass).

--- server/index.php
+++ server/index.php
@@ -5,6 +5,8 @@
  
 global $Notices;
 global $db;
+global $MainExecuted;
+$MainExecuted = false;
 
 define(DEBUG, false);
 
@@ -25,11 +27,82 @@ $db = new EmergencieDatabase;
  * Require Request Class and Instantiate
  */
 require_once('library/class.request.php');
-$request = new EmergencieRequest;
+//$request = new EmergencieRequest;
 
 
+/*
+ * Pass test data to Request object
+ 
+ 
+$emie_actions_test = array('LatLongToLocal', 'LatLongToLocal');
+$emie_parameters_test = array(
+	array('lat' => 53.298056, 'long' => -2.988281 ),	// Liverpool
+	array('lat' => 38.897676, 'long' => -77.036530 ),	// White House, Washington DC
+);
 
-PrettyPrint($_REQUEST);
+PrettyPrint($emie_actions);
+PrettyPrint($emie_parameters);
+*/
+
+/*
+ * Loop through data, creating instantiated objects
+ */
+function MainExecuteRequest($emie_actions, $emie_parameters) {
+	global $MainExecuted;
+	if(sizeof($emie_actions) == sizeof($emie_parameters)) :
+	
+		$i = 0;
+
+		foreach($emie_actions as $action) {
+			
+			//generate unique name based on iteration of loop
+			$individual_variable_name = 'request' . $i; 
+			
+			//instantiated class with variable name
+			$$individual_variable_name = new EmergencieRequest($emie_actions[$i]);
+			
+			
+			
+			//generate unique name for URL return variable
+			$individual_request_url_name = $individual_variable_name . '_url';
+			
+			//assign parameters using iteration count to match the reflected arrays
+			$$individual_request_url_name = $$individual_variable_name->RequestParameters($emie_parameters[$i]);
+			
+			
+			
+			//create individual output variable name
+			$individual_output_name = $individual_variable_name . '_output';
+			
+			//get execute return, assign to unique variable
+			$$individual_output_name = $$individual_variable_name->Execute($$individual_request_url_name);
+			
+			echo '<h3>'.$i.'</h3>';
+			PrettyPrint(($$individual_variable_name));
+			PrettyPrint(($$individual_request_url_name));
+			PrettyPrint(json_decode($$individual_output_name));
+			
+			$i++;
+		}
+	else :
+		echo 'Mismatched Actions and Parameters arrays - the lengths are different! Corrupt data request, whole request ignored.';
+	endif;
+	$MainExecuted = true;
+}
+if(!$MainExecuted && isset($emie_actions_test) && isset($emie_parameters_test))
+	MainExecuteRequest($emie_actions_test, $emie_parameters_test);
+
+
+/*
+$LatLongToLocal = new EmergencieRequest('LatLongToLocal');
+$parameters = array('lat' => 53.298056, 'long' => -2.988281 );
+$ResuestURL = $LatLongToLocal->RequestParameters($parameters);
+PrettyPrint($LatLongToLocal->Execute($ResuestURL));
+
+$request_notices = $LatLongToLocal->GetNotices();
+*/
+
+PrettyPrint($request_notices);
 
 
 ShowNotices();
--- server/library/class.request.php
+++ server/library/class.request.php
@@ -6,8 +6,11 @@
 class EmergencieRequest {
 	
 	public $request_type;
+	public $request_notices = NULL;
 	protected $allowed_request_types = array('LatLongToLocal',
 											);
+											
+	protected $geonames_username = 'harribellthomas';
 	
 	
 	/**
@@ -28,4 +31,125 @@ class EmergencieRequest {
 		} else 
 			return FALSE;
 	}
+
+
+
+	/**
+	 * Retrieve Request Notices
+	 *
+	 * @return string array $request_notices if notices are present, or FALSE if all is sparky.
+	 */
+	function GetNotices() {
+		if($this->request_notices)
+			return $this->request_notices;
+		else
+			return FALSE;
+	}
+
+
+
+	/**
+	 * Assign parameters and generate request URL.
+	 *
+	 * @param string array $parameters Array of parameters to be parsed.
+	 * @return string $RequestURL, valid and to be executed.
+	 */
+
+	function RequestParameters($parameters = NULL) {
+		$RequestURL = '';
+		switch ($this->request_type) :
+		
+			/**
+	 		 * Case: LatLongToLocal
+			 *
+			 * @param float/integer $parameters['lat'] (Latitude for Request), float/integer $parameters['long'] (Longitude for Request)
+			 */
+			case 'LatLongToLocal' :
+				if($parameters) :
+					if(isset($parameters['lat']) && isset($parameters['long'])) {
+						if($this->AreLatAndLongValid($parameters['lat'], $parameters['long']))
+							$RequestURL = 'http://api.geonames.org/findNearbyPlaceNameJSON?lat='.$parameters["lat"].'&lng='.$parameters["long"];
+						else
+							$this->request_notices[] = 'LatLongToLocal - lat and long set but not valid.';	
+					} else {
+						$this->request_notices[] = 'LatLongToLocal - lat and long not set, but parameters variable passed.';	
+					}
+				else :
+					$this->request_notices[] = 'LatLongToLocal - no parameters set.';
+				endif;
+				break;
+		
+		endswitch;
+		
+		if($RequestURL !== '') $RequestURL = $RequestURL . '&username=' . $this->geonames_username;
+		
+		return $RequestURL;
+	}
+	
+	
+	/**
+	 * Execute Generated URL
+	 *
+	 * @param string $URL URL to be cURL-ed
+	 * @return string $output, cURL return data
+	 */
+
+	function Execute($URL) {
+		
+		$output = FALSE;
+		
+		if(isset($URL) && filter_var($URL, FILTER_VALIDATE_URL)) :
+		
+			$ch = curl_init(); 
+			curl_setopt($ch, CURLOPT_URL, $URL); 
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+			$output = curl_exec($ch); 
+			curl_close($ch);    
+
+		else:
+			$this->request_notices[] = 'Execute - URL passed is NOT valid. No cURL retrieval attempted. URL is ' . $URL;
+		endif;
+		
+		return $output;
+	}
+	
+	
+		
+	/**
+	 * Are Latitude and Longitude Values Valid?
+	 *
+	 * @param float/integer $lat (Latitude), float/integer $long (Longitude)
+	 * @return TRUE if both params are valid, FALSE if not.
+	 */
+
+	function AreLatAndLongValid($lat, $long) {
+		$valid = true; // innocent until proved guilty
+		
+		if(!is_numeric($lat) || (int)$lat > 90 || (int)$lat < -90 )
+			$valid = false;
+		if(!is_numeric($long) || (int)$long > 180 || (int)$long < -180 )
+			$valid = false;
+			
+		return $valid;
+	}
+	
+	
+		
+	/**
+	 * Convert Latitude and Longitude to Postal Code
+	 *
+	 * @param float/integer $lat (Latitude), float/integer $long (Longitude)
+	 * @return TRUE if both params are valid, FALSE if not.
+	 */
+
+	function LatLongToPostCode($lat, $long) {
+		if(isset($lat) && isset($long)) {
+			if( $this->AreLatAndLongValid($lat, $long) ) {
+				$RequestURL = 'http://api.geonames.org/findNearbyPostalCodesJSON?lat='.$lat.'&lng='.$long.'&username='. $this->geonames_username;
+			}
+		} else { 
+			return 'Latitude and Longitude not set.';
+		} 
+	}
+		
 }
