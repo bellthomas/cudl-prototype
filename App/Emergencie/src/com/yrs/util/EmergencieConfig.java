494fd5a38e57bca6b62d9d72a71f55aa364a0114
JCTyblaidd <james@tyblaidd.co.uk>
2014-07-28 22:04:11 +0100

    Implemented Save to Configuration
    Reflection to save registering of all the different types, all calling
    it should add a try/catch to pick up any errors caused (shouldn't be any
    though)

--- App/Emergencie/src/com/yrs/util/EmergencieConfig.java
+++ App/Emergencie/src/com/yrs/util/EmergencieConfig.java
@@ -1,5 +1,7 @@
 package com.yrs.util;
 
+import java.io.IOException;
+
 /**
  * 
  * Simple data set class for instructions to be carried out by the phone
@@ -16,6 +18,7 @@ public class EmergencieConfig {
 	}
 	
 	
+	
 	public void Execute() {
 		//Run Location Service
 		location.DefaultPinPoint();
@@ -27,8 +30,27 @@ public class EmergencieConfig {
 		}
 	}
 	
+	public SaveUtil getSaveUtil() {
+		SaveUtil save = new SaveUtil();
+		save.buffer.putInt(instructionSet.length);
+		for(IEmergencieInstruction instrn : instructionSet) {
+			save.putString(instrn.getClass().toString());
+			save.putSaveUtil(instrn.getSave());
+		}
+		return save;
+	}
 	
-	
+	@SuppressWarnings("rawtypes")
+	public static EmergencieConfig loadFromSaveUtil(SaveUtil save) throws IOException, ReflectiveOperationException {
+		EmergencieConfig config = new EmergencieConfig(new IEmergencieInstruction[save.buffer.getInt()]);
+		for(int i = 0; i < config.instructionSet.length; i++) {
+			Class clazz = Class.forName(save.getString());
+			IEmergencieInstruction instruction = (IEmergencieInstruction) clazz.newInstance();
+			instruction.loadFromSaveUtil(save.getSaveUtil());
+			config.instructionSet[i] = instruction;
+		}
+		return config;
+	}
 	
 	
 	
@@ -36,26 +58,64 @@ public class EmergencieConfig {
 	//Type Classes
 	public static interface IEmergencieInstruction {
 		public void Execute(EmergencieConfig config);
+		public SaveUtil getSave(); //Save EVERYTHING
+		public void loadFromSaveUtil(SaveUtil save);
 	}
 	
 	public static class CallEmergencieInstruction implements IEmergencieInstruction{
+		
 		@Override
 		public void Execute(EmergencieConfig config) {
 			//TODO
 		}
+		
+		@Override
+		public SaveUtil getSave() {
+			return null; //TODO
+		}
+		
+		@Override
+		public void loadFromSaveUtil(SaveUtil save) {
+			//TODO
+		}
+		
 	}
 	
 	public static class TextEmergencieInstruction implements IEmergencieInstruction{
+
 		@Override
 		public void Execute(EmergencieConfig config) {
 			//TODO
 		}
+		
+		@Override
+		public SaveUtil getSave() {
+			return null; //TODO
+		}
+		
+		@Override
+		public void loadFromSaveUtil(SaveUtil save) {
+			//TODO
+		}
+		
 	}
 	
 	public static class EmailEmergencieInstruction implements IEmergencieInstruction{
+
 		@Override
 		public void Execute(EmergencieConfig config) {
 			//TODO
 		}
+		
+		@Override
+		public SaveUtil getSave() {
+			return null; //TODO
+		}
+		
+		@Override
+		public void loadFromSaveUtil(SaveUtil save) {
+			//TODO
+		}
+		
 	}
 }
